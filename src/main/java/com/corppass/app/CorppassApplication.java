599fe63ec6981cb975726866109a350e2c6a60d0
heindeveloper <heinhtetaungcu@gmail.com>
2020-03-20 11:36:21 +0630

    first commit

---
+++ src/main/java/com/corppass/app/CorppassApplication.java
@@ -0,0 +1,13 @@
+package com.corppass.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorppassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorppassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corppass/app/CorppassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corppass.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorppassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
